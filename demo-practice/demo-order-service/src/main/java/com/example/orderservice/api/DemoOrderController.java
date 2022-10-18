ce0f9999a350b0d947e38e14c267471c9d700171
FangYuan <61506177+FangYuan33@users.noreply.github.com>
2022-10-18 17:17:00 +0800

    验证seata完毕

--- demo-practice/demo-order-service/src/main/java/com/example/orderservice/api/DemoOrderController.java
+++ demo-practice/demo-order-service/src/main/java/com/example/orderservice/api/DemoOrderController.java
@@ -2,13 +2,17 @@ package com.example.orderservice.api;
 
 import com.example.orderservice.service.DemoGoodsService;
 import com.example.orderservice.service.DemoShoppingCartService;
+import io.seata.core.exception.TransactionException;
 import io.seata.spring.annotation.GlobalTransactional;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
+@Slf4j
 @RestController
 public class DemoOrderController {
 
@@ -17,7 +21,6 @@ public class DemoOrderController {
     @Resource
     private DemoShoppingCartService demoShoppingCartService;
 
-    @GlobalTransactional
     @GetMapping("/order/saveOrder")
     public String consumerTest(@RequestParam("cartId") int cartId, @RequestParam("goodsId") int goodsId) {
         // 调用商品服务
@@ -27,5 +30,18 @@ public class DemoOrderController {
         String cartResult = demoShoppingCartService.getCartItemDetail(cartId);
 
         return "success! goodsResult={" + goodsResult + "},cartResult={" + cartResult + "}";
+    }
+
+    @Transactional
+//    @GlobalTransactional
+    @GetMapping("/order/testSeata")
+    public void testSeata(@RequestParam("cartId") long cartId, @RequestParam("goodsId") long goodsId) throws TransactionException {
+        log.info("Test Seata: cartId:{}, goodsId:{}", cartId, goodsId);
+
+        demoGoodsService.delete(goodsId);
+
+        int i = 1 / 0;
+
+        demoShoppingCartService.delete(cartId);
     }
 }
