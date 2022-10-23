92a916c91bde765bd299e8ac3e9fe9bd51c4f715
FangYuan <61506177+FangYuan33@users.noreply.github.com>
2022-10-23 10:25:54 +0800

    initial gate-way and user-service

--- fy-mall-cloud/gateway-admin/src/main/java/com/cloud/mall/fy/FyMallCloudGatewayAdminApplication.java
+++ fy-mall-cloud/gateway-admin/src/main/java/com/cloud/mall/fy/FyMallCloudGatewayAdminApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FyMallCloudApplication {
+public class FyMallCloudGatewayAdminApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FyMallCloudApplication.class, args);
+        SpringApplication.run(FyMallCloudGatewayAdminApplication.class, args);
     }
 
 }
---
+++ fy-mall-cloud/gateway-admin/src/test/java/com/cloud/mall/fy/FyMallCloudGatewayAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.mall.fy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FyMallCloudGatewayAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
