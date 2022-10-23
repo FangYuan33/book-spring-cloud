a5ad1c72a9a41f7647da87c326b31823685e7f39
FangYuan <61506177+FangYuan33@users.noreply.github.com>
2022-10-23 10:15:42 +0800

    initial fy-mall-cloud

---
+++ fy-mall-cloud/src/main/java/com/cloud/mall/fy/FyMallCloudApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.mall.fy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FyMallCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FyMallCloudApplication.class, args);
+    }
+
+}
---
+++ fy-mall-cloud/src/test/java/com/cloud/mall/fy/FyMallCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.mall.fy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FyMallCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
