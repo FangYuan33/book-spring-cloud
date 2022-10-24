14764c5639f3e3346780edadba9e58426db406a8
FangYuan <61506177+FangYuan33@users.noreply.github.com>
2022-10-24 16:38:16 +0800

    user-service: 更改全局异常处理器的位置

--- fy-mall-cloud/common/src/main/java/com/cloud/mall/fy/common/exception/GlobalExceptionHandler.java
+++ fy-mall-cloud/common/src/main/java/com/cloud/mall/fy/common/exception/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
-package com.cloud.mall.fy.userservice.config;
+package com.cloud.mall.fy.common.exception;
 
 import com.cloud.mall.fy.common.dto.Result;
-import com.cloud.mall.fy.common.exception.BusinessException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
