4a97813498801c9e1970fa0f905efa5cb819392e
FangYuan <61506177+FangYuan33@users.noreply.github.com>
2022-11-02 20:55:11 +0800

    goods-service: 写了一个集合copy的工具

--- fy-mall-cloud/common/src/main/java/com/cloud/mall/fy/common/utils/BeanUtils.java
+++ fy-mall-cloud/common/src/main/java/com/cloud/mall/fy/common/utils/BeanUtils.java
@@ -1,14 +1,49 @@
 package com.cloud.mall.fy.common.utils;
 
+import com.cloud.mall.fy.common.exception.BusinessException;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
 public class BeanUtils {
 
-    public static Object copyProperties(Object source, Object target, String... ignoreProperties) {
-        if (source == null) {
-            return target;
+    /**
+     * copy目的类型的集合对象
+     *
+     * @param sources 原集合
+     * @param clazz   目的集合类
+     */
+    public static <T> List<T> copyList(List<?> sources, Class<T> clazz) {
+        List<T> targetList = new ArrayList<>();
+        if (sources != null) {
+            for (Object source : sources) {
+                T target = copyProperties(source, clazz);
+                targetList.add(target);
+            }
         }
-        org.springframework.beans.BeanUtils.copyProperties(source, target, ignoreProperties);
-        return target;
+        return targetList;
     }
 
+    /**
+     * 赋值出一个目的类的对象
+     *
+     * @param source           原对象
+     * @param targetClass      目的类
+     * @param ignoreProperties 忽略的字段
+     */
+    public static <T> T copyProperties(Object source, Class<T> targetClass, String... ignoreProperties) {
+        try {
+            T result = targetClass.newInstance();
+            if (source != null) {
+                org.springframework.beans.BeanUtils.copyProperties(source, result, ignoreProperties);
+            }
 
+            return result;
+        } catch (InstantiationException | IllegalAccessException e) {
+            log.error(e.getMessage(), e);
+            throw new BusinessException(e.getMessage());
+        }
+    }
 }
