84edb1f384c59a915a1c5a8eea853ef37036fd93
FangYuan <61506177+FangYuan33@users.noreply.github.com>
2022-11-10 11:07:07 +0800

    没用的删掉

--- fy-mall-cloud/goods-service/goods-api/src/main/java/com/cloud/mall/fy/goodsapi/openfeign/GoodsServiceOpenFeign.java
+++
@@ -1,8 +0,0 @@
-package com.cloud.mall.fy.goodsapi.openfeign;
-
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient(value = "goods-service", path = "/goods")
-public interface GoodsServiceOpenFeign {
-
-}
